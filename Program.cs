e383445ef56b438e650caf691fbcf6edb44d2677
chuangmaster <chuang.master@gmail.com>
2024-03-26 02:57:37 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Demo_Appsetting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Runtime.CompilerServices;
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args).ConfigureHostConfiguration(configHost =>
+{
+    //使用環境變數來參數檔的設定，進而使appsettings.json 、 appsettings.{Environment}.json 設定被覆蓋
+    configHost.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+});
+
+
+IHost host = builder.ConfigureServices((hostContext, services) =>
+{
+    services.AddSingleton<IWorker, Worker>();
+})
+.Build();
+
+Console.WriteLine($"Environment variable is: {Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}");
+Console.WriteLine(host.Services.GetRequiredService<IHostEnvironment>().EnvironmentName);
+Console.WriteLine("--------- Service start ---------");
+IWorker worker = host.Services.GetService<IWorker>();
+
+worker.Run();
+
---
+++ Worker.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Appsetting
+{
+    interface IWorker
+    {
+        void Run();
+    }
+    internal class Worker : IWorker
+    {
+        public IConfiguration _Configuration { get; set; }
+        public Worker(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Work Hard");
+            Console.WriteLine($"{_Configuration["Key"]}");
+
+            //foreach (var config in _Configuration.AsEnumerable())
+            //{
+            //    Console.WriteLine($"{config.Key}: {config.Value}");
+            //}
+        }
+    }
+}
